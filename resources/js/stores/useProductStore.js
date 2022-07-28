57f2f0dbe7b67833e0812e686b355145a719a5d6
HBilal Khan Yousafzai <hbilal_9@hotmail.com>
2022-07-28 11:54:53 +0500

    product api will call only if product array empty

--- resources/js/stores/useProductStore.js
+++ resources/js/stores/useProductStore.js
@@ -17,8 +17,12 @@ export const useProductStore = defineStore('productStoreId', {
 
     actions: {
         setProducts: async function (){
-            const response = await http().get('products');
-            this.products = response.data;
+            if(this.products.length == 0){
+                const response = await http().get('products');
+                this.products = response.data;
+            }else{
+                return this.products;
+            }
         }
     }
 })
